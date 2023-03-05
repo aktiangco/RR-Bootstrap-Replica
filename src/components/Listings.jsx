ecc3c2ee1b33d82b61faf8f32a031f1fa0ebd029
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-03-04 20:57:40 -0800

    Listing template

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,11 +1,77 @@
-
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const Listings = () => {
     return (
-        <div>
-            <h2> Listings</h2>
-            <p>Cards</p>
-        
+        <div style={{ margin: "auto"}}>
+                <h2 style={{textAlign: "center"}}>Store Listing</h2>
+            <Container style={{ marginTop: "40px", textAlign: "center" }}>
+                <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} >
+                    <Row>
+                        <Col xs=".5" >
+                            <Image src="[IMG SRC]" roundedCircle />
+                        </Col>
+                        <Col>
+                            <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
+                            <p>Delivery * Pickup</p>
+                        </Col>
+                    </Row>
+                    <Col sm="1"></Col>
+                    <Row >
+                        <Col xs=".5">
+                            <Image src="[IMG SRC]" roundedCircle />
+                        </Col>
+                        <Col>
+                            <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
+                            <p>Delivery * Pickup</p>
+                        </Col>
+                    </Row>
+                    <Col sm="1"></Col>
+                    <Row >
+                        <Col xs=".5" >
+                            <Image src="[IMG SRC]" roundedCircle />
+                        </Col>
+                        <Col md="6">
+                            <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
+                            <p>Delivery</p>
+                        </Col>
+                    </Row>
+                </Row>
+
+                <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} >
+                    <Row>
+                        <Col xs=".5" >
+                            <Image src="[IMG SRC]" roundedCircle />
+                        </Col>
+                        <Col>
+                            <h5 style={{ fontWeight: "bold" }}>H-E-B</h5>
+                            <p>Delivery</p>
+                        </Col>
+                    </Row>
+                    <Col sm="1"></Col>
+                    <Row >
+                        <Col xs=".5">
+                            <Image src="[IMG SRC]" roundedCircle />
+                        </Col>
+                        <Col>
+                            <h5 style={{ fontWeight: "bold" }}>Sam's club</h5>
+                            <p>Delivery</p>
+                        </Col>
+                    </Row>
+                    <Col sm="1"></Col>
+                    <Row >
+                        <Col xs=".5" >
+                            <Image src="[IMG SRC]" roundedCircle />
+                        </Col>
+                        <Col md="6">
+                            <h5 style={{ fontWeight: "bold" }}>Costco</h5>
+                            <p>Delivery</p>
+                        </Col>
+                    </Row>
+                </Row>
+            </Container>        
         </div>
     )
 }
