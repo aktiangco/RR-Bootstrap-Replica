f7274107a061dd4c043f5bc02377abc8d4add3e8
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-03-04 20:35:07 -0800

    background semi done. cant color match

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,7 +1,30 @@
+import { Form } from "react-bootstrap"
+import { Card } from "react-bootstrap"
+import BACKLogo from '../images/homepage_background.jpeg'
+
 const Background = () => {
     return (
-        <div>
-            <h2> Background Here</h2>
+        <div className="p-2" style={{ display: "flex", flexDirection: "row" }}>
+            <div style={{backgroundColor: "rgb(237, 250, 230)", width: "100%"}}>
+                <Card style={{ margin: "", padding: "40px", border: "none", backgroundColor: "transparent", width: "600px"}}>    
+                <Card.Text className="p-2" style={{ fontSize: "2.5em", fontWeight: "bold"}}>
+                    Order groceries for delivery
+                    <br/>
+                    or pick up today
+                <Card.Text style={{margin: "0", padding: "0", fontSize: "14px", fontWeight:"initial"}}>
+                    Whatever you want from local store4s, brought right to your door
+                </Card.Text>
+                </Card.Text>
+                <Form className="p-2">
+                    <Form.Group className="mb-3" >
+                        <Form.Control size="lg" type="text" placeholder="   Enter your address                                                      ->" />
+                    </Form.Group>
+                </Form>
+            </Card>
+            </div>
+            <div>
+                <img src={BACKLogo} alt="food" />
+            </div>
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,65 +5,79 @@ import Nav from 'react-bootstrap/nav';
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import Logo from '../images/instacart-logo.svg'
+import Logo from '../images/logo-dark.webp'
 
 
 const NavBar = () => {
     
     return (
-        <div className="" style={{width: "100%"}}>
+        <div className="" style={{width: "100%",backgroundColor: "rgb(52, 80, 72)" }}>
             {[false].map((expand) => (
                 <Navbar key={expand} bg="light" expand={expand} className="mb-3">
                     <Container
-                        fill
-                        style={{flexWrap: "none"}}>
-                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                        <Navbar.Offcanvas
-                        id={`offcanvasNavbar-expand-${expand}`}
-                        aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                        placement="start"
-                        >
-                            <Offcanvas.Header closeButton>
-                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                                Offcanvas
-                                </Offcanvas.Title>
-                            </Offcanvas.Header>
-                            <Offcanvas.Body>
-                                <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link href="#action1">Home</Nav.Link>
-                                <Nav.Link href="#action2">Link</Nav.Link>
-                                <Nav.Link href="#action1">Link</Nav.Link>
-                                <Nav.Link href="#action2">Link</Nav.Link>
-                                </Nav>                    
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
+                      fluid
+                        style={{ flexWrap: "none",backgroundColor: "rgb(52, 80, 72)", width: "100%" }}
+                        className=""
+                    >
                         <Navbar.Brand
                             className="align-self-start"
-                            style={{ margin: "5px 0 0 20px" }}
+                            style={{ margin: "10px 0 0 20px", textAlign: "start"}}           
                             href="#home"
                         >
+                        
+                            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                            <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="start"
+                            >
+                                <Offcanvas.Header closeButton>
+                                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    <Navbar.Brand
+                                        className="align-self-start"
+                                        style={{ margin: "5px 0 0 20px" }}
+                                        href="#home"
+                                    >
+                                        <img className="p-2" src={Logo} alt="logo" />
+                                    </Navbar.Brand>
+                                    </Offcanvas.Title>
+                                </Offcanvas.Header>
+                                <Offcanvas.Body>
+                                    <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="#action1">Home</Nav.Link>
+                                    <Nav.Link href="#action2">Link</Nav.Link>
+                                    <Nav.Link href="#action1">Link</Nav.Link>
+                                    <Nav.Link href="#action2">Link</Nav.Link>
+                                    </Nav>                    
+                                </Offcanvas.Body>
+                            </Navbar.Offcanvas>
+                        
                             <img src={Logo} alt="logo" />
                         </Navbar.Brand>
-                        <Navbar.Collapse
+                        <Navbar
                             id="basic-navbar-nav"
                             className=""
                             style={{ borderRadius: '10px', margin: "5px 0 0 20px", display: "flex" }}
                         >
-                            <Nav className="mr-auto" style={{ margin: "5px 0 0 20px" }}>
-                                <Form className="d-flex">
+                            <Nav
+                                fill
+                                className="mr-auto "
+                                style={{ margin: "5px 0 0 20px"}}>
+                                <Form className="d-flex" style={{width: "500px"}}>
                                     <Form.Control
                                         type="search"
-                                        placeholder="Search"
+                                        placeholder="Search products, stores and recipes"
                                         className="me-2"
                                         aria-label="Search"
                                     />
-                                    <Button variant="outline-success">Search</Button>
+                                    <Button variant="outline-success">
+                                        Search
+                                    </Button>
                                 </Form>
                             </Nav>
-                            <Nav
-                                style={{ color: "white", textDecoration: "none", margin: "5px 0 0 100px", fontSize: "15px", fontWeight: "bold" }}
-                            >
+                            <Nav>
                                 <Nav.Link
+                                    style={{color: "white", backgroundColor: "rgb(61, 128, 61)", borderRadius: "20px", textDecoration: "none", margin: "5px 0 0 100px", fontSize: "15px", fontWeight: "bold", width: "80px", textAlign: "center"}}
                                     href="#LogIn"
                                 >
                                     Log In
@@ -71,11 +85,12 @@ const NavBar = () => {
                             </Nav>
                             <Button
                                 variant="success"
-                                style={{ borderRadius: '10px', margin: "5px 0 0 20px", fontSize: "15px", fontWeight: "bold" }}
+                                style={{ borderRadius: '20px', margin: "5px 0 0 20px", fontSize: "15px", fontWeight: "bold" }}
                             >
                                 Sign Up
                             </Button>
-                        </Navbar.Collapse>
+                        </Navbar>
+                        
                     </Container>
                 </Navbar>   
             ))}    
